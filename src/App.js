38043672e7c5043588d268bd3d3cb624ed5e5a97
Linn Johansson <linn.johansson@bliwa.se>
2023-03-07 11:38:06 +0100

    initial app -  The Starting Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h2> Hallå Let's get started!</h2>
+      <div style={{color: 'pink'}}> 
+        Hallå Let's get started!
+      </div>
+    </div>
+  );
+}
+
+export default App;
