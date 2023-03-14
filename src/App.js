60d08b6866c03f023312ec343ffdd89131c9e098
Linn Johansson <linn.johansson@bliwa.se>
2023-03-14 11:50:34 +0100

    Building a First Custom Component
    https://pro.academind.com/courses/1366761/lectures/31414241

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import {ExpenseItem, minLista} from "./components/ExpenseItem";
+
 function App() {
+
+// let listan  = minLista.map(
+//   nummer => <span>{{nummer}}</span>
+// );
+
   return (
     <div>
       <h2> Hallå Let's get started!</h2>
       <div style={{color: 'pink'}}> 
         Hallå Let's get started!
       </div>
+
+
+      <ExpenseItem></ExpenseItem>
+
+      {/* <div>min lista : 
+       
+        
+        {minLista.map(
+          nummer => <span key={nummer}>{{nummer}}</span>
+        )}
+
+        </div> */}
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,28 @@
+function ExpenseItem () {
+    return (
+        <div>
+            <h1>Expense Item</h1>
+        </div>
+    );
+}
+
+function ExpenseTwo() {
+    return(
+        <div>expense två </div>
+    )
+}
+// export ExpenseTwo;
+
+
+const minLista = [
+    "1","2","3"
+]
+
+// export minLista;
+export default ExpenseItem;
+
+export {
+    minLista, 
+    ExpenseTwo,
+    ExpenseItem
+}
